e3f62c986e5d87a78fd081e233afb5abea101324
eastten <account.kk@gamil.com>
2021-06-05 13:57:21 +0900

    Initial Commit

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugin: ["stylelint-scss"],
+  extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
+  rules: {
+    "font-family-no-missing-generic-family-keyword": null,
+    "at-rule-no-unknown": null, //scssで使える @include などにエラーがでないようにする
+    "no-descending-specificity": null,
+  },
+};
